711a15f7b38ffbce5378a7260fb41c0e918dbd4d
Jackson Chen <jacksonchen171@gmail.com>
2020-02-29 22:52:21 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(input)
   index = input.to_i - 1
 end
 
-def move(board, index, value = "X")
-  board[index] = value
+def move(board, input, value = "X")
+  board[input] = value
 end
